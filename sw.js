aa653dd7b5feb5b55af0d7b3a1301282b457d5c0
Muhammad Saqib Zahid <m.saqibzahid.iqbal@gmail.com>
2026-03-02 13:48:18 +0500

    Converted to PWA

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'uni-time-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './icon.svg',
+  'https://cdn.tailwindcss.com',
+  'https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:wght@400;500;600;700;800&display=swap',
+  'https://unpkg.com/lucide@latest'
+];
+
+// Install Service Worker and cache assets
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Serve cached content when offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
